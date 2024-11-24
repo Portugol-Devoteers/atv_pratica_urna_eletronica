42db369c8cfe3127cdc0b4a3e3f25ccc8cd7f745
lucascarmon4 <lucascarmonaneto510@gmail.com>
2024-11-24 12:04:19 -0300

    salvando resultados em arquivo pkl

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     root = tk.Tk()
     urna = UrnaEletronica(root)
     def ao_fechar():
-        urna.salvar_votos_em_txt()
+        urna.salvar_votos_em_pkl()
         root.destroy()
     root.protocol("WM_DELETE_WINDOW", ao_fechar)
     root.mainloop()
